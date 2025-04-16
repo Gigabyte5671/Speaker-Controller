c969f33cb4a6a788beeecc2732dca161a2bf1bf5
Giga <gigabyte5671@gmail.com>
2025-04-16 14:28:36 +1200

    Create the UI

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,8 +1,172 @@
 <script setup lang="ts">
+import { ref } from 'vue';
+import Switch from './Switch.vue';
+
+const connected = ref(false);
+const enabled = ref(false);
+const showSettings = ref(false);
 </script>
 
 <template>
+	<main>
+		<div class="background">
+			<div class="layer-1"></div>
+			<div class="layer-2"></div>
+		</div>
+
+		<Switch :connected v-model="enabled" />
+
+		<div class="row">
+			<div class="status">
+				<span class="led" :class="{ connected }"></span>
+				<span class="text">Connected</span>
+			</div>
+			<button class="settings" :class="{ showSettings }" @click="showSettings = !showSettings">
+				<img src="./assets/settings_24dp_FFFFFF_FILL1_wght400_GRAD0_opsz24.svg" alt="">
+			</button>
+		</div>
+
+		<div class="lighting">
+			<div class="layer-1"></div>
+			<div class="layer-2" :class="{ connected, enabled }"></div>
+		</div>
+	</main>
 </template>
 
 <style scoped>
+main {
+	display: flex;
+	flex-flow: column nowrap;
+	gap: 20px;
+	width: 100vw;
+	height: 100vh;
+	text-align: center;
+	user-select: none;
+}
+
+main > .background {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+
+	& > * {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+
+	& .layer-1 {
+		background: url('./assets/Poliigon_MetalPaintedMatte_7037/Poliigon_MetalPaintedMatte_7037_Normal.png');
+		background-position: center;
+		background-size: 700%;
+		filter: grayscale(1) brightness(0.29) contrast(1.3);
+	}
+
+	& .layer-2 {
+		background: url('./assets/Poliigon_MetalPaintedMatte_7037/Poliigon_MetalPaintedMatte_7037_BaseColor.png');
+		background-position: center;
+		background-size: 700%;
+		mix-blend-mode: color-dodge;
+	}
+}
+
+.lighting {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	overflow: hidden;
+	pointer-events: none;
+
+	& > * {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+	}
+
+	& .layer-1 {
+		background: linear-gradient(180deg, #eeeb, #444b);
+		mix-blend-mode: soft-light;
+		opacity: 0.5;
+	}
+
+	& .layer-2.connected.enabled {
+		background: radial-gradient(circle at 50% 115px, #f97 20%, transparent 90%);
+		mix-blend-mode: soft-light;
+		opacity: 0.2;
+	}
+}
+
+.row {
+	display: flex;
+	justify-content: space-between;
+	padding: 0 14px 16px 20px;
+}
+
+.status {
+	display: flex;
+	align-items: center;
+	gap: 5px;
+
+	& .led {
+		width: 18px;
+		height: 18px;
+		background: radial-gradient(circle at center, #fff, #f46 30%, #000 50%);
+		border-radius: 50%;
+		filter: grayscale(0.85) brightness(0.7);
+
+		&.connected {
+			filter: grayscale(0) brightness(1);
+		}
+	}
+
+	& .text {
+		font-size: 14px;
+		font-style: italic;
+		letter-spacing: 1px;
+		text-shadow: 0 1px 1px currentColor;
+		text-transform: uppercase;
+		mix-blend-mode: soft-light;
+	}
+}
+
+button {
+	display: grid;
+	place-items: center;
+	text-align: center;
+	background: linear-gradient(180deg, #262626, #202020);
+	border: unset;
+	border-radius: 6px;
+	box-shadow:
+		0 3px 0 0 #080808,
+		0 4px 8px -4px #000;
+	outline: unset;
+	overflow: hidden;
+
+	&:is(:hover, :focus-visible) img {
+		opacity: 0.4;
+	}
+
+	&:active {
+		box-shadow:
+			0 1px 0 0 #080808,
+			0 4px 8px -4px #000;
+		translate: 0 2px;
+	}
+
+	&.settings {
+		padding: 5px;
+		background-color: transparent;
+
+		&.showSettings img {
+			opacity: 1;
+			filter: drop-shadow(0 1000px 0 #4acfff) drop-shadow(0 0 3px #ddf7);
+  			transform: translateY(-1000px);
+		}
+	}
+
+	& img {
+		opacity: 0.3;
+	}
+}
 </style>
---
+++ app/src/Switch.vue
@@ -0,0 +1,181 @@
+<script setup lang="ts">
+import { ref, watch } from 'vue';
+
+defineProps<{
+	connected: boolean;
+	modelValue: boolean;
+}>();
+
+const emit = defineEmits<{
+	'update:modelValue': [ boolean ];
+}>();
+
+const enabled = ref(false);
+
+watch(enabled, value => emit('update:modelValue', value));
+</script>
+
+<template>
+	<label for="switch" class="switch" :class="{ connected, enabled }">
+		<input type="checkbox" id="switch" name="switch" v-model="enabled">
+		<span class="light"></span>
+		<span class="bulb"></span>
+		<span class="rocker">
+			<span class="background"></span>
+			<span class="off"></span>
+			<span class="on"></span>
+		</span>
+	</label>
+</template>
+
+<style scoped>
+.switch {
+	display: grid;
+	place-items: center;
+	width: 150px;
+	height: 150px;
+	margin: auto;
+	background-color: #000;
+	background:
+		url('./assets/noise.png'),
+		radial-gradient(circle at 50% 75%, transparent, #fff1),
+		radial-gradient(circle at center, #000 53.5%, #222 calc(53.5% + 1px), #222 61%, #030303 calc(61% + 1px));
+	background-size:
+		cover,
+		auto,
+		auto;
+	background-blend-mode: difference;
+	border-radius: 50%;
+	box-shadow:
+		0 4px 8px -4px #000,
+		0 3px 12px -3px #0006;
+
+	& > * {
+		position: absolute;
+	}
+
+	& input {
+		display: none;
+	}
+
+	& .light {
+		width: calc(100% - 40px);
+		height: calc(100% - 40px);
+		background:
+			url('./assets/noise.png'),
+			radial-gradient(circle at center, #fdd, #fba 10%, #f97 50%, transparent),
+			radial-gradient(circle at 50% 10%, transparent 50%, #f55 calc(50% + 1px));
+		border-radius: 50%;
+		filter: brightness(0.2) grayscale(0.4);
+	}
+
+	& .bulb {
+		width: 22px;
+		height: 22px;
+		background: radial-gradient(circle at center, #fdd, #fba 10%, #f97 50%, transparent);
+		border-radius: 50%;
+		filter: blur(3px) brightness(0.4) grayscale(0.6);
+	}
+
+	& .rocker {
+		display: flex;
+		flex-flow: column nowrap;
+		justify-content: center;
+		align-items: center;
+		grid-template-rows: 1fr 1fr;
+		gap: 30px;
+		width: calc(100% - 50px);
+		height: calc(100% - 55px);
+		background:
+			url('./assets/noise.png'),
+			radial-gradient(circle at 50% 100%, #0002 30%, #ddd2),
+			radial-gradient(circle at 50% 130%, #0003 60%, #ddd3);
+		border-radius: 50%;
+		box-shadow:
+			0 0 4px 0 #fff4,
+			0 0 2px 0 #fff6 inset;
+		scale: 1 0.95;
+		translate: 0 -6.5px;
+
+		& .background {
+			position: absolute;
+			width: 87%;
+			height: 87%;
+			background: radial-gradient(2px 2px at 2px 2px, #fff2, transparent);
+			background-position: -1px -1px;
+			background-size: 4px 4px;
+			border-radius: inherit;
+			clip-path: polygon(
+				0 0,
+				100% 0,
+				100% calc(50% - 6px),
+				calc(100% - 6px) calc(50% - 6px),
+				calc(100% - 6px) calc(50% + 6px),
+				100% calc(50% + 6px),
+				100% 100%,
+				0 100%,
+				0 calc(50% + 6px),
+				6px calc(50% + 6px),
+				6px calc(50% - 6px),
+				0 calc(50% - 6px)
+			);
+			mix-blend-mode: overlay;
+		}
+
+		& .on {
+			width: 4px;
+			height: 22px;
+			background-color: #fffe;
+			border-radius: 2px;
+			mix-blend-mode: overlay;
+		}
+
+		& .off {
+			width: 20px;
+			height: 20px;
+			border: 4px solid #fffe;
+			border-radius: 50%;
+			mix-blend-mode: overlay;
+		}
+	}
+
+	&.enabled {
+		& .light {
+			background:
+				url('./assets/noise.png'),
+				radial-gradient(circle at 50% 55%, transparent 69%, #fff1 10%),
+				radial-gradient(25% 7% at 50% 6%, #fdc5 40%, transparent),
+				radial-gradient(circle at center, #fdd, #fba 10%, #f97 50%, transparent),
+				radial-gradient(circle at 50% 90%, transparent 50%, #f556 calc(50% + 1px));
+		}
+
+		& .rocker {
+			background:
+				url('./assets/noise.png'),
+				radial-gradient(circle at 50% 0%, #0002 30%, #ddd1),
+				radial-gradient(circle at 50% -30%, #0003 60%, #ddd1);
+			translate: 0 6.5px;
+		}
+	}
+
+	&.connected.enabled {
+		& .light {
+			background:
+				url('./assets/noise.png'),
+				radial-gradient(circle at 50% 55%, transparent 69%, #fff1 10%),
+				radial-gradient(circle at center, #fdd, #fba 10%, #f97 50%, transparent),
+				radial-gradient(circle at 50% 50%, transparent 50%, #d007 calc(50% + 1px));
+			filter: brightness(0.8) grayscale(0);
+		}
+
+		& .bulb {
+			filter: blur(3px) brightness(1.2) grayscale(0);
+		}
+
+		& .rocker {
+			background:
+				radial-gradient(circle at 50% 0%, #0002 65%, #ddd2);
+		}
+	}
+}
+</style>
